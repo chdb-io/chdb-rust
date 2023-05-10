317c8e495ceeb99dd5dd120f0bea5ec619447874
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-05-10 19:17:57 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = args.get(1).unwrap_or(&"SELECT version()".to_string());
-    let format = args.get(2).unwrap_or(&"CSV".to_string());
+    let query = args.get(1).map_or("SELECT version()".to_string(), |arg| arg.to_string());
+    let format = args.get(2).map_or("CSV".to_string(), |arg| arg.to_string());
 
-    match bindings::execute(query, format) {
+    match bindings::execute(&query, &format) {
         Some(result) => println!("{}", result),
         None => println!("Query execution failed."),
     }
