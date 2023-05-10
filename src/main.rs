8fd7368fab8da1a614b6c7a67cbe8e3b5d0644a9
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-05-10 19:14:38 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod bindings;
 
+use std::env;
+
 fn main() {
-    let query = "SELECT version()";
-    let format = "CSV";
+    let args: Vec<String> = env::args().collect();
+    let query = args.get(1).unwrap_or(&"SELECT version()".to_string());
+    let format = args.get(2).unwrap_or(&"CSV".to_string());
 
     match bindings::execute(query, format) {
         Some(result) => println!("{}", result),
         None => println!("Query execution failed."),
     }
 }
-
